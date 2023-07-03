777e4d46099b8f6be13040cc10a8701c007c508a
hamza1216 <hymtmk1106@gmail.com>
2023-07-03 10:49:18 +0200

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    browser: true,
+  },
+  globals: {
+    module: true,
+    JSX: 'readonly',
+    stripe: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    'object-curly-newline': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unused-prop-types': 'warn',
+    'react/prop-types': 'off',
+    'react/no-children-prop': 'off',
+    'dot-notation': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', {varsIgnorePattern: '_$', argsIgnorePattern: '_$'}],
+    '@typescript-eslint/no-var-requires': 'off',
+    'sort-imports': [
+      'warn',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['all', 'multiple', 'single', 'none'],
+        allowSeparatedGroups: true,
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	trailingComma: 'all',
+	singleQuote: true,
+	jsxSingleQuote: true,
+	bracketSpacing: false,
+	printWidth: 120,
+	parser: 'typescript',
+};
