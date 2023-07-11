98686dbd192743d0881c67a2f7ccef45aa2d4a1f
hamza1216 <hymtmk1106@gmail.com>
2023-07-11 10:21:16 +0200

    add search bar

---
+++ src/components/search.tsx
@@ -0,0 +1,15 @@
+import {MagnifyingGlassIcon} from '@heroicons/react/24/solid';
+import classNames from 'classnames';
+
+interface SearchBarProps {
+  className?: string;
+}
+
+export const SearchBar = ({className}: SearchBarProps) => {
+  return (
+    <div className={classNames('flex h-9 px-4 py-[6px] bg-content-black rounded-[48px] items-center', className)}>
+      <MagnifyingGlassIcon className='w-5 h-5 text-content-grey-400' />
+      <input className='ml-2 text-16 text-content-grey-400 bg-transparent outline-none' placeholder='Search' />
+    </div>
+  );
+};
