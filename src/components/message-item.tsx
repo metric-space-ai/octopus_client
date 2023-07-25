47e910180dad3f64259f3e45e74d638db1ffbfac
hamza1216 <hymtmk1106@gmail.com>
2023-07-25 14:39:26 +0200

    fix: refresh chat message

--- src/components/message-item.tsx
+++ src/components/message-item.tsx
@@ -1,14 +1,48 @@
+import {useCallback, useEffect} from 'react';
+
 import {UserCircleIcon} from '@heroicons/react/24/solid';
 import Image from 'next/image';
 
 import Logo44 from '@/assets/icons/logo-44.png';
+import {getChatMessageApi} from '@/services/chat.service';
+import {useChatStore} from '@/store';
 import {IChatMessage} from '@/types';
+
+import {AnimateDots} from './svgs';
 
 interface IMessageItem {
   item: IChatMessage;
 }
 
 export const MessageItem = ({item}: IMessageItem) => {
+  const {updateMessage} = useChatStore();
+  const loading = item.status === 'Asked';
+
+  const checkMessageResponse = useCallback(
+    (after: number) => {
+      setTimeout(() => {
+        getChatMessageApi(item.chat_id, item.id).then((res) => {
+          if (res.data.status === 'Asked') {
+            checkMessageResponse(2000);
+          } else {
+            updateMessage(res.data);
+          }
+        });
+      }, after);
+    },
+    [item.chat_id, item.id, updateMessage],
+  );
+
+  useEffect(() => {
+    if (item.status === 'Asked') {
+      const estimationResponseTime = new Date(item.estimated_response_at);
+      const now = new Date();
+      const diffTime = estimationResponseTime.valueOf() - now.valueOf();
+      checkMessageResponse(diffTime);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [item]);
+
   return (
     <div className='mt-3 text-15 font-medium'>
       <div className='flex gap-3 items-center'>
@@ -26,7 +60,7 @@ export const MessageItem = ({item}: IMessageItem) => {
           alt='logo'
         />
         <div className='py-4 px-5 bg-content-black rounded-[20px] rounded-tl-none'>
-          <p className='text-white'>{item.response}</p>
+          {loading ? <AnimateDots /> : <p className='text-white'>{item.response}</p>}
         </div>
       </div>
     </div>
