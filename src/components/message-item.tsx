1c43c8c4b71930e186e2d3364ac6e40563691136
hamza1216 <hymtmk1106@gmail.com>
2023-08-08 14:33:06 +0200

    fix: message item

--- src/components/message-item.tsx
+++ src/components/message-item.tsx
@@ -1,15 +1,13 @@
 import {useCallback, useEffect} from 'react';
 
-import {UserCircleIcon} from '@heroicons/react/24/solid';
-import Image from 'next/image';
+import {UserIcon} from '@heroicons/react/24/solid';
 
-import Logo44 from '@/assets/icons/logo-44.png';
 import {getChatMessageApi} from '@/services/chat.service';
 import {useChatStore} from '@/store';
 import {IChatMessage} from '@/types';
 
 import {MarkdownContent} from './markdown';
-import {AnimateDots} from './svgs';
+import {AnimateDots, LogoIcon} from './svgs';
 
 interface IMessageItem {
   item: IChatMessage;
@@ -47,20 +45,16 @@ export const MessageItem = ({item}: IMessageItem) => {
   return (
     <div className='mt-3 text-15 font-medium'>
       <div className='flex gap-3 items-center'>
-        <UserCircleIcon className='w-9 h-9 text-content-grey-900' />
+        <div className='w-9 h-9 flex items-center justify-center bg-content-black rounded-full'>
+          <UserIcon className='w-6 h-6 text-content-grey-100' />
+        </div>
         {item.message}
       </div>
       <div className='mt-3 flex gap-3'>
-        <Image
-          className='w-9 h-9 object-cover rounded-full'
-          width={0}
-          height={0}
-          priority
-          sizes='100vw'
-          src={Logo44}
-          alt='logo'
-        />
-        <div className='py-4 px-5 bg-content-black rounded-[20px] rounded-tl-none'>
+        <div className='w-9 h-9 flex items-center justify-center bg-content-black rounded-full'>
+          <LogoIcon width={22} height={20} color='#F5F5F5' />
+        </div>
+        <div className='flex-1 py-4 px-5 bg-content-black rounded-[20px] rounded-tl-none'>
           {loading ? <AnimateDots /> : <MarkdownContent content={item.response} />}
         </div>
       </div>
