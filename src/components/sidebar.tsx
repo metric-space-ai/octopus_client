60cf2bcb2df0979d56adfc10ee40fed5a4979c79
hamza1216 <hymtmk1106@gmail.com>
2023-08-28 11:56:52 +0200

    fix: content safety

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -20,7 +20,7 @@ const ChatList = dynamic(async () => (await import('./chat-list')).ChatList, {
 
 export function SideBar(props: {className?: string}) {
   const chatStore = useChatStore();
-  const [enabled, setEnabled] = useState(false);
+  const [enabled, setEnabled] = useState(true);
   const [expanded, setExpanded] = useState(true);
   const [showDeactivateConfirmationModal, setShowDeactivateConfirmationModal] = useState(false);
 
@@ -51,7 +51,7 @@ export function SideBar(props: {className?: string}) {
         <ChatList expanded={expanded} />
       </div>
       {expanded && (
-        <div className='mt-4'>
+        <div className='flex flex-col mt-4 gap-1'>
           <div className='flex justify-between'>
             <div className='flex items-center gap-1'>
               <ShieldCheckIcon className='w-5 h-5 text-content-accent-400' />
@@ -66,14 +66,20 @@ export function SideBar(props: {className?: string}) {
                   setEnabled(true);
                 }
               }}
-              className={`${enabled ? 'bg-content-accent' : 'bg-content-disabled'}
-        relative inline-flex h-6 w-12 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+              className={classNames(
+                `${
+                  enabled ? 'bg-content-accent' : 'bg-content-disabled'
+                } relative inline-flex h-6 w-12 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`,
+              )}
             >
               <span className='sr-only'>Use setting</span>
               <span
                 aria-hidden='true'
-                className={`${enabled ? 'translate-x-6' : 'translate-x-0'}
-          pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                className={classNames(
+                  `${
+                    enabled ? 'translate-x-6' : 'translate-x-0'
+                  } pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`,
+                )}
               />
             </Switch>
           </div>
