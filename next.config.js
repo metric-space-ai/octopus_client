22574a8673128826e146b025caac775414c57688
CyrusNejati <CyrusNejati@outlook.com>
2023-09-28 22:35:24 +0200

    add next config

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.octopus-ai.app',
+        port: '',
+        pathname: '/public/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
