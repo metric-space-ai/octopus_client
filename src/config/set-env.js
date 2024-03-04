91029a4ad83d09c7c3a0321016aa057fba906f26
CyrusNejati <CyrusNejati@outlook.com>
2024-03-04 13:41:52 +0100

    add env

--- src/config/set-env.js
+++ src/config/set-env.js
@@ -3,12 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
 
 var arg = process.env.NEXT_PUBLIC_BASE_URL;
+var arg2 = process.env.NEXT_PUBLIC_DOMAIN;
 
 // Set the environment variable in .env dynamically
-if (arg) {
-    (0, fs_1.writeFileSync)('.env', "NEXT_PUBLIC_BASE_URL=".concat(arg, "\n"));
-}
-else {
-    console.error('Error: REACT_APP_API_URL_ARG not provided.');
+if (arg && arg2) {
+    var content = "NEXT_PUBLIC_BASE_URL=".concat(arg, "\nNEXT_PUBLIC_DOMAIN=").concat(arg2, "\n");
+    (0, fs_1.writeFileSync)('.env', content);
+} else {
+    console.error('Error: NEXT_PUBLIC_DOMAIN or NEXT_PUBLIC_BASE_URL not provided.');
     process.exit(1);
 }
